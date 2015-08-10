b328fd59829d7da92e6496b7145c6ad4b8b8df0b
Bruno Lesieur <bruno.lesieur@gmail.com>
2015-08-10 12:00:15 +0200

    NodeAtlas index nice new look.
    Signed-off-by: Bruno Lesieur <bruno.lesieur@gmail.com>

--- node-atlas.js
+++ node-atlas.js
@@ -5,7 +5,7 @@
 /**
  * @fileOverview NodeAtlas allows you to create and manage HTML assets or create multilingual websites/webapps easily with Node.js.
  * @author {@link http://www.lesieur.name/ Bruno Lesieur}
- * @version 0.46.0
+ * @version 0.47.0
  * @license {@link https://github.com/Haeresis/ResumeAtlas/blob/master/LICENSE/ GNU GENERAL PUBLIC LICENSE Version 2}
  * @module node-atlas
  * @requires async
@@ -97,7 +97,7 @@ var NA = {};
         commander
 
             /* Version of NodeAtlas currently in use with `--version` option. */
-            .version('0.46.0')
+            .version('0.47.0')
 
             /* Automaticly run default browser with `--browse` options. If a param is setted, the param is added to the and of url. */
             .option(NA.appLabels.commander.browse.command, NA.appLabels.commander.browse.description, String)
@@ -2622,7 +2622,9 @@ var NA = {};
              * @type {Object}
              * @memberOf node-atlas~NA#currentVariation
              */
-            currentVariation.currentRouteName = path;
+            if (currentVariation.currentRouteParameters.url) {
+                currentVariation.currentRouteName = path;
+            }
 
             /**
              * Expose route of current page from current webconfig `routes`.
