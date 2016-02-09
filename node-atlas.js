7ccbbac267ff0452ec7299e2b19f861773e6ed44
Bruno Lesieur <bruno.lesieur@gmail.com>
2016-02-09 14:42:46 +0100

    Bin system provide by npm for install nodeatlas and nodeatlas.cmd file with bin directory.
    Signed-off-by: Bruno Lesieur <bruno.lesieur@gmail.com>

--- node-atlas.js
+++ node-atlas.js
@@ -5,7 +5,7 @@
 /**
  * @fileOverview NodeAtlas allows you to create and manage HTML assets or create multilingual websites/webapps easily with Node.js.
  * @author {@link http://www.lesieur.name/ Bruno Lesieur}
- * @version 1.1.4
+ * @version 1.1.9
  * @license {@link https://github.com/Haeresis/ResumeAtlas/blob/master/LICENSE/ GNU GENERAL PUBLIC LICENSE Version 2}
  * @module node-atlas
  * @requires {@link NA#modules.external:async}
