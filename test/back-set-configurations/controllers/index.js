197b2e988c0965bfcbf7df33ac7fb9c50919436c
Bruno Lesieur <bruno.lesieur@gmail.com>
2016-10-11 17:46:06 +0200

    Update README.md
    Signed-off-by: Bruno Lesieur <bruno.lesieur@gmail.com>

---
+++ test/back-set-configurations/controllers/index.js
@@ -0,0 +1,14 @@
+// On intervient avant que les variables soient injectées dans le système de template.
+// Ce code sera exécuté uniquement lors de la demande de la page « / ».
+exports.changeVariation = function (params, mainCallback) {
+    var variation = params.variation;
+
+    // On prépare le fichier à afficher un fichier json.
+    variation.currentRouteParameters.headers = {
+		"Content-Type": "application/json; charset=utf-8"
+    };
+    variation.content = JSON.stringify(variation, null, "    ");
+
+    // On ré-injecte les modifications.
+    mainCallback(variation);
+};
