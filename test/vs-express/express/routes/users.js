9dca07c215a51ed708a55796b7270c54c808911b
Bruno Lesieur <bruno.lesieur@gmail.com>
2017-01-14 17:27:09 +0100

    Update express exemple
    Signed-off-by: Bruno Lesieur <bruno.lesieur@gmail.com>

--- test/vs-express/express/routes/users.js
+++ test/vs-express/express/routes/users.js
@@ -1,22 +1,21 @@
 var express = require("express"),
-    router = express.Router();
-
-router.get("/(:member/)?", function(req, res) {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.send(`{
+    router = express.Router(),
+    send = function (req) {
+      return `{
   "params": "${req.params.member}",
   "query": "${req.query.member}"
   "body": "${req.body.member}"
-}`);
+}`
+    };
+
+router.get("/(:member/)?", function (req, res) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.send(send(req));
 });
 
-router.post("/(:member/)?", function(req, res) {
+router.post("/(:member/)?", function (req, res) {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.send(`{
-  "params": "${req.params.member}",
-  "query": "${req.query.member}"
-  "body": "${req.body.member}"
-}`);
+  res.send(send(req));
 });
 
 module.exports = router;
